02fe6c92c9419852f0e0e2eaf4895a928a241304
Husien vora <www.husien.vora@gmail.com>
2022-12-18 14:33:58 +0530

    initial commit

--- backend/controllers/getAllImages.js
+++
@@ -1,8 +0,0 @@
-const imageModel = require("../models/Image-model");
-const getAllImages = async (req, res) => {
-  const allData = await imageModel.find();
-  res.send(allData);
-};
-module.exports = {
-  getAllImages,
-};
